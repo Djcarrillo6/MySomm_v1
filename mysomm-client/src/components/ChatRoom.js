e6dc4d4fbe36977c7d908a37654ac0f05c25ae58
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-09-07 15:11:01 -0700

    mods

--- mysomm-client/src/components/ChatRoom.js
+++ mysomm-client/src/components/ChatRoom.js
@@ -6,7 +6,8 @@ import { render } from 'react-dom';
 const ChatRoom = (props) => {
     return (
         <div>
-            <h1>Chat Room Socket</h1>
+            <h1>Chat Room Socket.io</h1>
+            <h2>Import from media</h2>
         </div>
     );
 }
