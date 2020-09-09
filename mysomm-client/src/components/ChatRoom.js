25ab7b8e3ca49b03e65216fd0f4f76fda70e17c3
David Carrillo Jr <djcarrillo6@yahoo.com>
2020-09-09 15:14:34 -0700

    mods

--- mysomm-client/src/components/ChatRoom.js
+++ mysomm-client/src/components/ChatRoom.js
@@ -7,7 +7,7 @@ const ChatRoom = (props) => {
     return (
         <div>
             <h1>Chat Room Socket.io</h1>
-            <h2>I</h2>
+            <h2>Web Socket Plug In for this component</h2>
         </div>
     );
 }
